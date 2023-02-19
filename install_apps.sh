fe752c9d220ee33201b82de2bb22faf67616feda
pHamala <petri.hamalainen@mailbox.org>
2023-02-19 14:00:33 +0200

    Removed comments from Signal- and VScode-repositories

--- install_apps.sh
+++ install_apps.sh
@@ -17,61 +17,61 @@ exec 1>$DATE.app_installation.log 2>&1
 
 echo "Updating system"
 
-sudo pkcon refresh && pkcon -y update
+sudo apt update && sudo apt full-upgrade -y
 
 echo "Installing essential packages"
 
 sudo apt install software-properties-common apt-transport-https wget build-essential flatpak -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-## Installing Signal ##
+## Install Signal ##
 
 echo "Installing Signal"
 
-#wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
-#cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
 
-#echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
-#sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
 
 sudo apt update && sudo apt install signal-desktop -y
 
-## Installing VS Code ##
+## Install VS Code ##
 
 echo "Installing VS Code"
 
-#wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 
-#sudo add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 
 sudo apt install code -y
 
-## Installing android-studio
+## Install android-studio
 
 sudo add-apt-repository ppa:maarten-fonville/android-studio -y
 sudo apt update
 sudo apt install android-studio
 
-## Installing Thunderbird ##
+## Install Thunderbird ##
 
 flatpak install -y --system flathub org.mozilla.Thunderbird
 
-## Installing Telegram
+## Install Telegram
 
 flatpak install -y --system flathub org.telegram.desktop
 
-## Installing Digikam
+## Install Digikam
 
 flatpak install -y --system flathub org.kde.digikam
 
-## Installing Steam
+## Install Steam
 
 flatpak install -y --system flathub com.valvesoftware.Steam
 
-## Installing Firefox and remove native version
+## Install Firefox as flatpak and remove native version
 
 flatpak install -y --system flathub org.mozilla.firefox
-sudo pkcon remove firefox
+sudo apt remove firefox
 
 
 
